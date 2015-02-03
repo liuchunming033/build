5ba18d4069060f7fd0fa3bb37bb354eb13d69310
liu.chunming <liu.chunming@irdeto.com>
2015-02-03 11:35:08 +0800

    new unit.py

---
+++ unit.py
@@ -0,0 +1,38 @@
+'''
+Created on Feb 2, 2015
+
+@author: liu.chunming
+'''
+import unittest
+
+class Widget:
+    def __init__(self, size = (40, 40)):
+        self._size = size
+    
+    def getSize(self):
+        return self._size
+
+    def resize(self, width, height):
+        if width < 0 or height < 0:
+            raise ValueError, "illegal size"
+        self._size = (width, height)
+
+    def dispose(self):
+        pass
+    
+class WidgetTestCase(unittest.TestCase):
+    def setUp(self):
+        self.widget = Widget()
+    
+    def testSize(self):
+        self.assertEqual(self.widget.getSize(), (40, 40))
+        
+    def tearDown(self):
+        self.widget = None
+        
+    def testResize(self):
+        self.widget.resize(100, 100)
+        self.assertEqual(self.widget.getSize(), (100, 100))
+
+if __name__ == "__main__":
+    unittest.main()
